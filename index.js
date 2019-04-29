e85f94814be5ed5bed55b0265faff3b1592d5048
Stuart Romanek <stuart@punkave.com>
2019-04-29 11:54:19 -0400

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+var _ = require('@sailshq/lodash');
+
+module.exports = {
+  moogBundle: {
+    directory: 'lib/modules',
+    modules: [
+      'apostrophe-personas-areas',
+      'apostrophe-personas-custom-pages',
+      'apostrophe-personas-pages',
+      'apostrophe-personas-widgets',
+      'apostrophe-personas-doc-type-manager',
+      'apostrophe-personas-pieces'
+    ]
+  }
+};
